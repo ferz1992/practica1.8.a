21c18c03fa394ee55327cba7043f616b4b34f924
Fernando Zalazar <f.zalazar@live.com>
2022-04-26 10:19:15 -0300

    practica10

---
+++ algoritmos.js
@@ -0,0 +1,13 @@
+
+
+  // problema 10
+
+  var velocidadInicial = 0;
+  var aceleracion = 20;
+  var velocidadFinal = 60;
+  var tiempoParaAlcanzarVelFinal = "no definido" ;
+
+  var tiempoParaAlcanzarVelFinal = velocidadInicial + aceleracion * tiempoParaAlcanzarVelFinal ;
+
+  console.log (tiempoParaAlcanzarVelFinal)
+  
